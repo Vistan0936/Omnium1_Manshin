86b940aea6bee3f7471315a1e34c80da5dd57ba5
Andrei <andreimanshin0936@gmail.com>
2024-11-09 09:55:38 +0600

    - init

---
+++ Assets/Scripts/Character/Components/Live/ImmortalLiveComponent.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmortalLiveComponent : ILiveComponent
+{
+    float ILiveComponent.MaxHealth { get => 1; }
+    float ILiveComponent.Health { get => 1; }
+
+    public event Action<Character> OnCharacterDeath;
+
+    public void Initialize(Character selfCharacter)
+    {
+        // throw new NotImplementedException();
+    }
+
+    public void SetDamage(float damage)
+    {
+        Debug.Log("I am immortal");
+    }
+}
